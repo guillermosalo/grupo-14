6fbe4f07e42fa778cea3949efc9805c0aed1102c
guillermosalo <guillermosalo@gmail.com>
2023-02-15 02:02:46 -0300

    Start create api
    Inicio proyecto en java estructurando para model, repository, service y controller

---
+++ src/main/java/DH/grupo14/Grupo14Application.java
@@ -0,0 +1,15 @@
+package DH.grupo14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo14Application.class, args);
+	}
+
+}
+
+//Ojo, log4j.properties, line 22 cargar db sql correctamente y borrar h2
